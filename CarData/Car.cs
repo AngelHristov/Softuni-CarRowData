af153743a318c02b44991fc0a03ce7eaedb0e673
Angel Hristov <angelxristov84@abv.bg>
2019-07-08 20:26:06 +0300

    Add project files.

---
+++ CarData/Car.cs
@@ -0,0 +1,29 @@
+﻿namespace P01_RawData
+{
+    using System.Collections.Generic;
+
+    public class Car
+    {
+        private string model;
+
+        public List<Tire> Tires { get; set; }
+
+        public string Model
+        {
+            get
+            { return this.model; }
+            set
+            { this.model = value; }
+        }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+
+        public Car(string model, Engine engine, Cargo cargo, Tire firstTire, Tire secondTire, Tire thirdTire, Tire fourthTire)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Cargo = cargo;
+            this.Tires = new List<Tire>() { firstTire, secondTire, thirdTire, fourthTire };
+        }
+    }
+}
---
+++ CarData/Engine.cs
@@ -0,0 +1,38 @@
+﻿namespace P01_RawData
+{
+    public class Engine
+    {
+        private int engineSpeed;
+        private int enginePower;
+
+        public int EngineSpeed
+        {
+            get
+            {
+                return this.engineSpeed;
+            }
+            set
+            {
+                engineSpeed = value;
+            }
+        }
+
+        public int EnginePower
+        {
+            get
+            {
+                return this.enginePower;
+            }
+            set
+            {
+                this.enginePower = value;
+            }
+        }
+
+        public Engine(int engineSpeed, int enginePower)
+        {
+            this.EngineSpeed = engineSpeed;
+            this.EnginePower = enginePower;
+        }
+    }
+}
